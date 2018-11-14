5008f52aabc021691f8b2273bc270f0ae6b7dd9e
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2018-11-14 14:14:24 +0100

    progress - 181114-14.14
    progress;

---
+++ java/TicTacToe/Programm/src/m226b/ifz724004a/tictactoe/SpielFeld.java
@@ -0,0 +1,16 @@
+package m226b.ifz724004a.tictactoe;
+
+public class SpielFeld {
+	public void spielFeld() {
+		System.out.println("");
+		System.out.println("       |     |     ");
+		System.out.println("    7  |  8  |  9  ");
+		System.out.println("  _____|_____|_____");
+		System.out.println("       |     |     ");
+		System.out.println("    4  |  5  |  6  ");
+		System.out.println("  _____|_____|_____");
+		System.out.println("       |     |     ");
+		System.out.println("    1  |  2  |  3  ");
+		System.out.println("       |     |     ");
+	}
+}
---
+++ java/TicTacToe/Programm/src/m226b/ifz724004a/tictactoe/TicTacToe.java
@@ -0,0 +1,12 @@
+package m226b.ifz724004a.tictactoe;
+
+import m226b.ifz724004a.utils.ConsoleReader;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		SpielFeld feld = new SpielFeld();
+		feld.spielFeld();
+	}
+
+}
