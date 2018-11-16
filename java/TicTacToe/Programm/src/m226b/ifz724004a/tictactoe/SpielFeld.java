0305548b145b09e26bf8d39bebb1cc0466bef33a
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2018-11-16 12:02:46 +0100

    progress - 181116-12.03
    progress spielFeld;

--- java/TicTacToe/Programm/src/m226b/ifz724004a/tictactoe/SpielFeld.java
+++ java/TicTacToe/Programm/src/m226b/ifz724004a/tictactoe/SpielFeld.java
@@ -4,14 +4,14 @@ public class SpielFeld extends Item {
 	
 	public void spielFeld() {
 		System.out.println("");
-		System.out.println("       |     |     ");
-		System.out.println("    "+ f7 +"  |  "+ f8 +"  |  "+ f9 +"  ");
-		System.out.println("  _____|_____|_____");
-		System.out.println("       |     |     ");
-		System.out.println("    "+ f4 +"  |  "+ f5 +"  |  "+ f6 +"  ");
-		System.out.println("  _____|_____|_____");
-		System.out.println("       |     |     ");
-		System.out.println("    "+ f1 +"  |  "+ f2 +"  |  "+ f3 +"  ");
-		System.out.println("       |     |     ");
+		System.out.println("       |     |     			Benutzung des 					     |     |     ");
+		System.out.println("       |     |     			Numpads Empfohlen:				  7  |  8  |  9  ");
+		System.out.println("  _____|_____|_____											_____|_____|_____");
+		System.out.println("       |     |     											     |     |     ");
+		System.out.println("       |     |     											  4  |  5  |  6  ");
+		System.out.println("  _____|_____|_____											_____|_____|_____");
+		System.out.println("       |     |     											     |     |     ");
+		System.out.println("       |     |     											  1  |  2  |  3  ");
+		System.out.println("       |     |     											     |     |     ");
 	}
 }
