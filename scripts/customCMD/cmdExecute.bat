6445e9d119ac866a1b428dee99928a91b045e64b
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2018-12-05 16:14:35 +0100

    progress - 181205-16.14
    customized cmd startet;

---
+++ scripts/customCMD/cmdExecute.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "D:\DATA\IT\Github\nemo-doku\scripts\customCMD\cmdLogo.bat"
---
+++ scripts/customCMD/cmdLogo.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set ESC=
+set Red=%ESC%[91m
+set White=%ESC%[37m
+
+echo %Red%     An
+echo      nAnnnn
+echo      nnnAnnnnnn
+echo      nnnnAnnnnnnnnn
+echo      nnnnnnAnnnnnnnnnnn											IP-Adresse:
+echo      nnnnnnnAnnnnnnnnnnnnnn
+echo      n             nnnnnnnnnnnn
+echo                     nnnnnnnnnnnnnnn								MAC-Adresse:
+echo          nnnnnnn    nnnnnnnnnnnnnnnnnnn
+echo          nnnnnnn    nnnnnnnnnnnnnnnnnnnnnnn
+echo          nnnnnnn    NNNNNNNNNNNNNNNNNNNNNNNNNNN
+echo          nnnnnnn    nnnnnnnnnnnnnnnnnnnnnnn
+echo          nnnnnnn    nnnnnnnnnnnnnnnnnnn
+echo          nnnnnnV    nnnnnnnnnnnnnnn
+echo          nnnnnVn    nnnnnnnnnnn
+echo      nnnnnnnVnnnnnnnnnnnnnn
+echo      nnnnnnVnnnnnnnnnnn
+echo      nnnnVnnnnnnnnn
+echo      nnnVnnnnnn
+echo      nVnnnn
+echo      Vn
+echo %White%
