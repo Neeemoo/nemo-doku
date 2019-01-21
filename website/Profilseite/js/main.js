e13972a53cfdcbdcda34806279cab61a5f6a59aa
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2019-01-21 11:14:09 +0100

    progress - 190121-11.14
    added a button to switch the iframe from phone view and desktop view;
    styled the iframe;

--- website/Profilseite/js/main.js
+++ website/Profilseite/js/main.js
@@ -2,3 +2,18 @@ function extend() {
   var mobile_nav = document.getElementById("gotocontent");
   mobile_nav.classList.toggle("weg");
 }
+
+function phone() {
+  iframe = document.getElementById("iframe");
+  iframe.classList.toggle("smaller");
+
+  button = document.getElementById("phone");
+  if (iframe.classList.contains("smaller")) {
+    button.innerHTML = "Desktop View";
+    document.getElementById("phone").style.width = "484px";
+  }
+  else {
+    button.innerHTML = "Mobile View";
+    document.getElementById("phone").style.width = "574px";
+  }
+}
