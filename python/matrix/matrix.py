248d589792e55c4061ffd07765f984c69a32a6f0
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2019-03-15 11:53:45 +0100

    progress - 190315-11.53
    started with matrix digital rain;

---
+++ python/matrix/matrix.py
@@ -0,0 +1,28 @@
+print()
+print("g")
+import time
+time.sleep(0.5)
+
+print("2")
+import time
+time.sleep(0.5)
+
+print("&")
+import time
+time.sleep(0.5)
+
+print("*")
+import time
+time.sleep(0.5)
+
+print("2")
+import time
+time.sleep(0.5)
+
+print("n")
+import time
+time.sleep(0.5)
+
+print("L")
+import time
+time.sleep(0.5)
