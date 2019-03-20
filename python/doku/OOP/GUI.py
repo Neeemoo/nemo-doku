4545b201843a2f7826bffebc0652dcddad1865e9
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2019-03-20 16:03:34 +0100

    progress - 190320-16.03
    changed colors;
    background color: black;
    text color: green;

--- python/doku/OOP/GUI.py
+++ python/doku/OOP/GUI.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
 root = Tk()
+root.configure(background='black')
 
-label = Label(root, text="Hello world")
+label = Label(root, text="Hello world", fg='#20C20E')
+label.configure(background='black')
 label.pack()
 
 root.mainloop()
