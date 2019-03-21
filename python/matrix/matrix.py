9e7c1073d3f8d1d5529ad7ac5696396c04889127
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2019-03-21 14:27:08 +0100

    progress - 190321-14.27
    label positioning;

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,15 +2,11 @@ from tkinter import *
 
 root = Tk()
 root.configure(background='black')
-w = 300
-h = 200
-x = 50
-y = 100
-root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+root.attributes('-fullscreen', True)
 
 label = Label(root, text='a', fg='#20C20E')
 label.configure(background='black')
+
 label.place(x=1, y=1)
-label.pack()
 
 root.mainloop()
